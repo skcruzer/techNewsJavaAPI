3542ae0e7a8f16f55f98cc1c763d2dc23efac4d6
skcruzer <96440269+skcruzer@users.noreply.github.com>
2022-05-08 22:27:03 -0700

    classes practice

---
+++ tech-news-java-api/src/main/java/com/technews/VariablePractices.java
@@ -0,0 +1,21 @@
+package com.technews;
+
+public class VariablePractices {
+    public static void main(String[] args) {
+        int a = 7;
+        double b = 3.5;
+        String c = "dog";
+
+//      calculated variable sum is a double
+        double sum = a + b;
+
+//      calculated variable concat is a String
+        String concat = a + c;
+
+//      value of sum is 10.5
+        System.out.println(sum);
+
+//      value of concat is "7dog"
+        System.out.println(concat);
+    }
+}

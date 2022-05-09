63e4c616ebba07678e458ec73a5da47d9b5202b2
skcruzer <96440269+skcruzer@users.noreply.github.com>
2022-05-08 22:52:36 -0700

    demo

---
+++ tech-news-java-api/src/main/java/com/technews/testModel/Demo.java
@@ -0,0 +1,11 @@
+package com.technews.testModel;
+
+public class Demo {
+    private String name;
+    private int age;
+
+    public Demo(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
